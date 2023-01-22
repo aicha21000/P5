c688c94a71f8d15f2c801d890ddc552020db8d2a
aicha21000 <salhi.aicha@hotmail.com>
2023-01-22 15:08:25 +0100

    multiply price

--- front/js/cart.js
+++ front/js/cart.js
@@ -23,7 +23,7 @@ if (cart != 0) {
     cartQuantity.value = `${cart[i].quantity}`;
 
     cartTotal.textContent = `${cart[i].quantity}`;
-    cartTotalPrice.textContent = `${cartQuantity.value * cart[i].price}`;
+    // cartTotalPrice.textContent = `${cartQuantity.value * cart[i].price}`;
 
     let codeAll = `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
         <div class="cart__item__img">
@@ -33,7 +33,7 @@ if (cart != 0) {
           <div class="cart__item__content__description">
             <h2 >${cartName.textContent}</h2>
             <p >${cartColor.textContent}</p>
-            <p >${cartPrice.textContent} </p>
+            <p class="priceNew">${cartPrice.textContent} </p>
           </div>
           <div class="cart__item__content__settings">
             <div class="cart__item__content__settings__quantity">
@@ -45,53 +45,94 @@ if (cart != 0) {
             </div>
           </div>
         </div>
+        <div>
+        
+        <div>
+
+
       </article> `;
 
     structure = structure + `${codeAll}`;
-
   }
 
   if (i === cart.length) {
     main.innerHTML = structure;
   }
-  
+
+  function totalQuantity1() {
+    let arr = document.querySelectorAll(".itemQuantity");
+    let tot = 0;
+    for (let l = 0; l < arr.length; l++) {
+      if (parseFloat(arr[l].value)) tot += parseFloat(arr[l].value);
+    }
+    console.log(tot);
+    cartTotal.innerText = tot;
+  }
+
+  totalQuantity1();
 
   for (let k = 0; k < cart.length; k++) {
     console.log(cart[k]);
     let cartDelete = document.querySelectorAll(".deleteItem")[k];
-      console.log("test")
+    console.log("test");
 
-  cartDelete.addEventListener("click", function () {
-   
+    cartDelete.addEventListener("click", function () {
       const index = cart.indexOf(cart[k]);
       if (index > -1) {
         cart.splice(index, 1);
-        localStorage.setItem('cart', JSON.stringify(cart));
+        localStorage.setItem("cart", JSON.stringify(cart));
         cart = JSON.parse(localStorage.getItem("cart"));
         console.log(index);
         window.location.reload();
 
-    
-
-console.log(cart);}})
-     
-
-    // cartQuantity.value = Number(`${cart[k].quantity}`);
+        console.log(cart);
+      }
+    });
 
     let itemQuantity = document.querySelectorAll(".itemQuantity")[k];
     itemQuantity.addEventListener("change", function () {
       console.log("test");
-    const cartQuantity = document.querySelectorAll(".itemQuantity")[k].value;
-    let numQuantity = parseInt(cartQuantity);
 
+      // quantity of the item in the cart
+      const itemQuantity = document.querySelectorAll(".itemQuantity")[k].value;
 
-      // cartTotalPrice.textContent = `${cartQuantity.cart[k] * cart[k].price}`;
-      // cartTotal.innerText += sumQuantity;
+      let numQuantity = Number(itemQuantity);
+
       console.log(numQuantity);
+
+      //  multiply price
+      let cartTotalPrice1 = `${numQuantity * cart[k].price}`;
+      console.log(cartTotalPrice1);
+      let priceNew = document.querySelectorAll(".priceNew")[k];
+      priceNew.innerText = `${cartTotalPrice1}`;
+
+      //   for (let j = 0; j < priceNew; j++) {
+
+      //     let cartTotalPrice1 = `${numQuantity * cart[k].price}`;
+      //   }
+      //   console.log(cartTotalPrice1);
+      //   cartTotal.innerText = tot;
+      // }
+
+      // total quantity
+
+      function totalQuantity() {
+        let arr = document.querySelectorAll(".itemQuantity");
+        let tot = 0;
+        for (let j = 0; j < arr.length; j++) {
+          if (parseFloat(arr[j].value)) tot += parseFloat(arr[j].value);
+        }
+        console.log(tot);
+        cartTotal.innerText = tot;
+      }
+
+      totalQuantity();
     });
-  }}
-  else  {
-   
+
+    // cartTotalPrice.textContent = `${cartQuantity.cart[k] * cart[k].price}`;
+  }
+} else {
   console.log("panier vide");
   main.innerHTML = `<div> <p> Votre panier est vide </p> </div>`;
-  }
+  cartTotal.innerText = "0";
+}
